51cdf6ead3dcba2df76f6810e1cf6f9820d4426f
thalia-moraesweb <moraesthalia.silva@gmail.com>
2022-04-27 16:57:52 -0300

    exercicio 3

---
+++ exercicios/exercicio03.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 3</title>
+</head>
+<body>
+    <?php
+    $salario = 500 ;
+   
+ 
+    
+
+ if( $salario <=500)  {
+     $reajuste = $salario*1.15;
+   
+    } elseif( ($salario >= 1000){
+        $reajuste1 = $salario*1.10
+      
+    } else { ($salario <= 1000) {
+        $salario*1.05
+    } 
+        
+
+        
+
+
+
+
+    ?>
+</body>
+</html>
