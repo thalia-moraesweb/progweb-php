091b596b5409c71e5bef9940bff380f01e6dd620
thalia-moraesweb <moraesthalia.silva@gmail.com>
2022-04-28 13:47:03 -0300

    exercicio 3

--- exercicios/exercicio03.php
+++ exercicios/exercicio03.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 3</title>
+    <style>
+        .reajuste { color: blue;}
+        .salario {color: red;}
+    </style>
 </head>
 <body>
     <?php
@@ -13,18 +17,18 @@
  
     
 
- if( $salario <=500)  {
+ if( $salario < 500)  {
      $reajuste = $salario*1.15;
    
-    } elseif( ($salario >= 1000){
-        $reajuste1 = $salario*1.10
+    } elseif ($salario <= 1000){
+        $reajuste = $salario * 1.10;
       
-    }else { ($salario <= 1000) {
-        $salario*1.05
-        echo "<p>reajuste</p>"
-    } 
+    } else  {
+       $reajuste = $salario * 1.05;
         
-     
+    } 
+      
+     echo "o salário antes do reajuste era <spam class = salario >$salario</spam> após o reajuste de 15% ficou em <spam class = reajuste>$reajuste</spam></spam>";
         
 
 
