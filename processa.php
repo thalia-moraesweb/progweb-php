0eddf5aa5476efb7f43e48e81daa241aea6507d9
thalia-moraesweb <moraesthalia.silva@gmail.com>
2022-05-05 15:44:06 -0300

    formulario

--- processa.php
+++ processa.php
@@ -8,5 +8,53 @@
 </head>
 <body>
     <h1>Processamento de Dados</h1>
-</body>
+    <hr>
+
+    <?php
+   /*  echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>"; */
+
+    // Capturando os dados enviados a partir do formulário
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $idade = $_POST["idade"];
+
+    /* Se existir o  $_POST['interesses'],
+    o que foi selecionado fica n a variavel. Caso contrario,
+    deixe um array vazio na variável.
+    
+    ?? Operador de coalescencia nula */
+    $interesses = $_POST["interesses"] ?? []; 
+    $informativos = $_POST["informativos"];
+    $mensagem = $_POST["mensagem"];
+    ?>
+    <h2>Dados:</h2>
+    <hr>
+    <ul>
+        <li> Nome: <?=$nome?> </li>
+        <li> Email: <?=$email?> </li>
+        <li> Idade: <?=$idade?> </li>
+
+        <!-- Aqui usamos o operador de negação (!) para inverter
+        a lógica de função empty. -->
+
+        <!-- Se interresses NÃO ESTIVER VAZIO -->
+        <?php if( !empty($interesses) ){  ?>
+        <!-- Então, faça tudo embaixo: -->
+        <li> interesses: 
+            <ul>
+            <?php foreach($interesses as $interesse){?> 
+                <li> <?=$interesse?> </li>
+                <?php } ?>
+                
+            </ul>
+            
+            <?php } ?>
+           
+    </li>
+        <li> Informativos: <?=$informativos?> </li>
+        <li> Mensagem: <?=$mensagem?> </li>
+        </ul>
+    </body>
 </html>
